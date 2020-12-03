5c966550d2704699728d6ddcf1de7b70bc64095a
MiGrossX <mixellydayanegross@gmail.com>
2020-12-03 11:07:53 -0300

    código

---
+++ src/script.js
@@ -0,0 +1,15 @@
+var likeslivro1 = 0;
+      var likeslivro2 = 0;
+      var likeslivro3 = 0;
+
+      function addlikelivro1() {
+        likeslivro1 = likeslivro1 + 1;
+        document.getElementById("likeslivro1").innerHTML = "Like" + likeslivro1;}
+
+      function addlikelivro2() {
+        likeslivro2 = likeslivro2 + 1;
+        document.getElementById("likeslivro2").innerHTML = "Like" + likeslivro2;}
+
+      function addlikelivro3() {
+        likeslivro3 = likeslivro3 + 1;
+        document.getElementById("likeslivro3").innerHTML = "Like" + likeslivro3;}
